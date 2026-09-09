c247f4bb5ea1f36800534cda25f41df4f8713044
jeongeun park <chorokje@gmail.com>
2026-09-09 10:24:47 +0900

    +0909 1.review problems complete

--- main.py
+++ main.py
@@ -20,10 +20,37 @@ def build_reply_chain(chat):
     #return 프롬프트 | 챗 | 파서 -> Str, Json, Structured
     return prompt | chat | StrOutputParser()
 
+#리뷰를 한 줄 한 줄 읽어서 build_reply_chain에 넣어주는 함수
+def generate_reply(reviews, chat):
+    reply_chain = build_reply_chain(chat)
+
+    #print(reviews)
+    for i in range(len(reviews)):
+        # print(review)
+        # print(review['label'])
+        # print(type(review['label']))
+        # #템플릿에서 뽑아올 2가지 요소
+        sentiment = '긍정' if reviews.loc[i]['label'] == 1 else "부정"
+        comment = reviews.loc[i]['comment']
+
+        # #{}이 안을 채워주세요!
+        result = reply_chain.invoke({'sentiment':sentiment, 
+                                     'comment':comment})
+
+        print(f'[답글 생성] 손님 댓글 {comment}\n 사장님 댓글 {result}')
+        
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv 
+#비밀 키를 가져오는 역할을 함 -> .env 
+load_dotenv()
 
 if __name__ == '__main__':
     #1. pandas 라이브러리를 사용해서 txt파일을 읽어오기
-    load_reviews('./tarr_train.txt')
+    df = load_reviews('./tarr_train.txt')
+
+    #temperature(0~1) : 창의성
+    chat = ChatOpenAI(temperature=0.7, model='gpt-4o')
+    generate_reply(df, chat)
 
 
     #2. 오늘 한 chain 함수를 이용해서 댓글 분류(선택) / 긍정-부정
